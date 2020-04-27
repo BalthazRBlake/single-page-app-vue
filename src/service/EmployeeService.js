3e150b90cc61371fd4f5e8d859c9ab3e4b8e8696
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-27 13:59:23 +0300

    consuming employee API with EmployeeService & axios

---
+++ src/service/EmployeeService.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const apiEmployee = axios.create({
+  baseURL: "https://spa-java-api.herokuapp.com/spapp/emp",
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
+export default {
+  getPaginatedEmployees(page, size) {
+    return apiEmployee.get("/paginated/" + page + "/" + size);
+  }
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,3 +3,25 @@
     <h1>Home</h1>
   </div>
 </template>
+
+<script>
+import EmployeeService from "@/service/EmployeeService.js";
+
+export default {
+  data() {
+    return {
+      employees: []
+    };
+  },
+  created() {
+    EmployeeService.getPaginatedEmployees(1, 10)
+      .then((response) => {
+        this.employees = response.data;
+        console.log(this.employees);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }
+};
+</script>
