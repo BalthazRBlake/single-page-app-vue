6f4b4854e97421b7c93a01eecd1835090e0f4ff9
FHHF <fabianhumbertohernandez@gmail.com>
2020-04-29 21:08:23 +0300

    format NavBar code

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,9 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
       <b-navbar-brand to="/">
-        <b-icon-code-slash class="d-inline-block align-top" font-scale="1.5"></b-icon-code-slash>Single Page Application Employees
+        <b-icon-code-slash class="d-inline-block align-top" font-scale="1.5">
+        </b-icon-code-slash>
+        Single Page Application Employees
       </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -11,15 +13,31 @@
         <b-navbar-nav>
           <b-nav-item :to="{ name: 'About' }">About</b-nav-item>
 
-          <b-nav-item href="https://github.com/BalthazRBlake/SinglePageAppFrontEnd">GitHub Repo</b-nav-item>
-          <b-nav-item href="https://spa-java-api.herokuapp.com">API Documentation</b-nav-item>
+          <b-nav-item
+            href="https://github.com/BalthazRBlake/SinglePageAppFrontEnd"
+          >
+            GitHub Repo
+          </b-nav-item>
+          <b-nav-item href="https://spa-java-api.herokuapp.com"
+            >API Documentation</b-nav-item
+          >
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="outline-info">Search</b-button>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button
+              size="sm"
+              class="my-2 my-sm-0"
+              type="submit"
+              variant="outline-info"
+              >Search</b-button
+            >
           </b-nav-form>
 
           <b-nav-item-dropdown text="Lang" right>
