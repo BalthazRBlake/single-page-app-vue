af1eb2575c7fd00e465f2e675e4f894a773103fd
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-10 17:29:29 +0300

    implementing Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    perPage: 10
+  },
+  mutations: {
+    SET_PER_PAGE(state, perPage){
+      this.perPage = perPage;
+    }
+  },
+  actions: {
+    changePerPage({ commit }, perPage){
+      commit("SET_PER_PAGE", perPage);
+    }
+  },
   modules: {}
 });
